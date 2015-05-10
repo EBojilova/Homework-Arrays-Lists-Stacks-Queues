47b4e74dcb945be86db080ea67fcb838d85f2d9a
EBojilova <elena_bogoeva2003@yahoo.co.uk>
2015-05-10 21:56:14 +0300

    Update CategorizeNumbers.cs

--- 03.CategorizeNumbers/CategorizeNumbers.cs
+++ 03.CategorizeNumbers/CategorizeNumbers.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
 
 class CategorizeNumbers
@@ -7,11 +6,16 @@ class CategorizeNumbers
     static void Main(string[] args)
     {
         double[] nums = Array.ConvertAll(Console.ReadLine().Split(' '), s => double.Parse(s));
-        var sorted = nums.GroupBy(i => i == (int)i);
-        var floatNums = sorted.First();
-        var roundNums = sorted.Last();
-        Console.WriteLine("[{0}] -> min: {1}, max: {2}, sum: {3}, avg: {4:F2}", string.Join(" ", floatNums), floatNums.Min(), floatNums.Max(), floatNums.Sum(), floatNums.Average());
-        Console.WriteLine();
-        Console.WriteLine("[{0}] -> min: {1}, max: {2}, sum: {3}, avg: {4:F2}", string.Join(" ", roundNums), roundNums.Min(), roundNums.Max(), roundNums.Sum(), roundNums.Average());
+        var floatNums = nums.Where(i => i != (int)i);
+        var roundNums = nums.Where(i => i == (int)i);
+        if (floatNums.Count()>0)
+        {
+            Console.WriteLine("[{0}] -> min: {1}, max: {2}, sum: {3}, avg: {4:F2}", string.Join(" ", floatNums), floatNums.Min(), floatNums.Max(), floatNums.Sum(), floatNums.Average());
+            Console.WriteLine();
+        }
+        if (roundNums.Count()>0)
+        {
+             Console.WriteLine("[{0}] -> min: {1}, max: {2}, sum: {3}, avg: {4:F2}", string.Join(" ", roundNums), roundNums.Min(), roundNums.Max(), roundNums.Sum(), roundNums.Average());
+        }
     }
 }
